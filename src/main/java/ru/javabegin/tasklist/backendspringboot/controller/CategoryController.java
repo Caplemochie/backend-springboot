539dfadf44ddfdbae448fd1e20f8d57af05bc1ff
Caplemochie <tagirza@yandex.ru>
2020-10-01 13:02:18 +0300

    проверка параметров для Priority

--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
@@ -1,12 +1,8 @@
 package ru.javabegin.tasklist.backendspringboot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.javabegin.tasklist.backendspringboot.entity.Category;
-import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.repo.CategoryRepository;
-import ru.javabegin.tasklist.backendspringboot.repo.PriorityRepository;
 
 import java.util.List;
 
@@ -32,6 +28,11 @@ public class CategoryController {
         System.out.println("list = " + list);
 
         return list;
+    }
+
+    @PostMapping("/add")
+    public Category add(@RequestBody Category category) {
+        return categoryRepository.save(category);
     }
 
 }
--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
@@ -1,9 +1,10 @@
 package ru.javabegin.tasklist.backendspringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.javabegin.tasklist.backendspringboot.entity.Category;
 import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.repo.PriorityRepository;
 
@@ -31,4 +32,22 @@ public class PriorityController {
         return list;
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+
+        // проверка на обязательные параметры
+        if (priority.getId() != null && priority.getId() != 0) {
+            // id создается автоматически в БД (autoincrement), поэтому его передавать не нужно, иначе может быть конфликт уникальности значения
+            return new ResponseEntity("returned param: id MUST be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        // если передали пустое значение title
+        if (priority.getTitle() == null && priority.getTitle().trim().length() == 0) {
+            return new ResponseEntity("missed param: title", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
 }
